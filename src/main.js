737d408e3fe06d51785effc196821f452ec8140b
KayoSavio <kayosavio@hotmail.com>
2022-02-17 20:21:24 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
-import VueTheMask from 'vue-the-mask';
 import router from './routes';
-import money from 'v-money';
-import InputFacade from 'vue-input-facade';
 import "./global.css";
 
 Vue.config.productionTip = false;
-Vue.use(VueTheMask);
-Vue.use(money, { precision: 2 });
-Vue.use(InputFacade);
 
 Vue.prototype.$bus = new Vue();
 new Vue({
